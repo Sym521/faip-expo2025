56b90ec19d54efeb1b763b302786cae5475b657d
sym521 <hiroki.seyama@outlook.jp>
2025-06-24 09:47:24 +0900

    feat: complete "flow" content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 					</p>
 					<Image
 						src="/faipIllust.png"
-						alt="Description of image"
+						alt="FAIP illustration"
 						width={180}
 						height={200}
 						className="absolute -bottom-9 right-4 object-cover"
@@ -33,35 +33,43 @@ export default function Home() {
 				id="overview"
 				className="my-6 scroll-mt-20 border-2 border-slate-200 bg-white/40 backdrop-blur-sm rounded-lg shadow-md mr-2 px-2 py-4"
 			>
-				<h2 className="text-xl font-bold text-[#b74e70] mb-2">
+				<h2 className="text-2xl font-bold text-[#b74e70] mb-2">
 					{t.menu.overview}
 				</h2>
-				<p className="text-blue-950/90 leading-relaxed">{t.overview}</p>
+				<p className="px-1 text-blue-950/90 leading-relaxed">{t.overview}</p>
+				<Image
+									src="/overview.png"
+									alt="Overview image"
+									width={500}
+									height={300}
+									quality={100}
+									className="mt-2 rounded-lg object-cover max-w-full"
+								/>
 			</section>
 			<section
 				id="flow"
 				className="my-6 scroll-mt-20 border-2 border-slate-200 bg-white/40 backdrop-blur-sm rounded-lg shadow-md mr-2 px-2 py-4"
 			>
-				<h2 className="text-xl font-bold text-[#b74e70] mb-2">{t.menu.flow}</h2>
+				<h2 className="text-2xl font-bold text-[#b74e70] mb-2">{t.menu.flow}</h2>
 				{t.flow_contents.map((line: string, idx: number) => {
 					// 画像挿入用: 文字列が [img:パス] の形式なら画像を表示
 					const imgMatch = line.match(/^\[img:(.+)\]$/);
 					if (imgMatch) {
 						return (
-							<div key={idx} className="my-4 flex justify-center">
+							<div key={idx} className="my-2 flex justify-center">
 								<Image
 									src={imgMatch[1]}
-									alt=""
+									alt="Description of flow"
 									width={500}
 									height={300}
 									quality={100}
-									className="rounded-lg shadow-md object-cover max-w-full"
+									className="mt-2 rounded-lg object-cover max-w-full"
 								/>
 							</div>
 						);
 					}
 					return (
-						<p key={idx} className="text-blue-950/90 leading-relaxed">{line}</p>
+						<p key={idx} className="px-1 text-blue-950/90 leading-relaxed">{line}</p>
 					);
 				})}
 			</section>
@@ -69,7 +77,7 @@ export default function Home() {
 				id="future"
 				className="my-6 scroll-mt-20 border-2 border-slate-200 bg-white/40 backdrop-blur-sm rounded-lg shadow-md mr-2 px-2 py-4"
 			>
-				<h2 className="text-xl font-bold text-[#b74e70] mb-2">{t.menu.future}</h2>
+				<h2 className="text-2xl font-bold text-[#b74e70] mb-2">{t.menu.future}</h2>
 				<p className="text-blue-950/90 leading-relaxed">{t.future}</p>
 			</section>
 			{/* footer */}
