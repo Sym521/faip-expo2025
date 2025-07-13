64b0a4c67718e2216024ef8b7940c22b11460925
sym521 <hiroki.seyama@outlook.jp>
2025-07-14 00:58:02 +0900

    fix: add typeof en

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -2,7 +2,7 @@ import ja from "../i18n/ja.json";
 import en from "../i18n/en.json";
 
 export type Lang = "ja" | "en";
-export type Messages = typeof ja;
+export type Messages = typeof ja | typeof en;
 
 const resources: Record<Lang, Messages> = { ja, en };
 
