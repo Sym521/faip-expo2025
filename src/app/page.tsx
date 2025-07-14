b92c863d61c7e8e8dc591f52954b714f722a8fe6
sym521 <hiroki.seyama@outlook.jp>
2025-07-14 18:40:31 +0900

    fix: fixed section id and some paragraphs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -122,7 +122,7 @@ export default function Home() {
 				})}
 			</section>
 			<section
-				id="currentWork"
+				id="currentWorks"
 				className="my-6 scroll-mt-20 border-2 border-slate-200 bg-white/40 backdrop-blur-sm rounded-lg shadow-md mr-2 px-2 py-4"
 			>
 				<h2 className="text-2xl font-bold text-myakupink mb-2">
